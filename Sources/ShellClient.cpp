2fa35b42c86aa600ee5239362e20355b56a15bcf
Jajauma <jajauma@2ch.hk>
2017-10-02 19:15:27 +0300

    Client and server stub

---
+++ Sources/ShellClient.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int
+main(int argc, char* argv[])
+{
+    std::cout << "Shell client stub" << std::endl;
+    return 0;
+}
---
+++ Sources/ShellServer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int
+main(int argc, char* argv[])
+{
+    std::cout << "Shell server stub" << std::endl;
+    return 0;
+}
