99029a4a0ac11e31e308e66e46f3bf4167bae31c
Jajauma <jajauma@2ch.hk>
2017-10-04 11:47:16 +0300

    Add Cxx::Unused

--- Application/ShellServer.cpp
+++ Application/ShellServer.cpp
@@ -1,8 +1,12 @@
+#include "Cxx/Unused.hpp"
+
 #include <iostream>
 
 int
 main(int argc, char* argv[])
 {
+    Cxx::Unused(argc);
+    Cxx::Unused(argv);
     std::cout << "Shell server stub" << std::endl;
     return 0;
 }
---
+++ Cxx/Unused.hpp
@@ -0,0 +1,12 @@
+#ifndef HEADER_FFC77506_5BFF_11E7_8FF1_74D435E5BEA6_INCLUDED
+#define HEADER_FFC77506_5BFF_11E7_8FF1_74D435E5BEA6_INCLUDED
+
+namespace Cxx {
+template <typename T>
+void
+Unused(T&&)
+{
+}
+} /* namespace Cxx  */
+
+#endif /* end of the header guard */
