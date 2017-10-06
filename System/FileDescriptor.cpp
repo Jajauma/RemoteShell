281408c0870c156d51b6943859678c09b15a0880
Jajauma <jajauma@2ch.hk>
2017-10-06 12:37:23 +0300

    RAII wrapper on unix file descriptors

---
+++ System/FileDescriptor.cpp
@@ -0,0 +1,76 @@
+#include "FileDescriptor.hpp"
+#include "Cxx/Assert.hpp"
+
+#include <cerrno>
+#include <iostream>
+#include <system_error>
+
+#include <unistd.h>
+
+#if defined(BUILD_UNIT_TESTS)
+#include <gtest/gtest.h>
+#endif /* BUILD_UNIT_TESTS */
+
+using namespace System;
+
+static constexpr FileDescriptor::Handle None = -1;
+
+FileDescriptor::FileDescriptor(Handle fd)
+    : mHandle{fd}
+{
+    CXX_VALIDATE_ARG(!(fd < 0));
+}
+
+FileDescriptor::FileDescriptor(FileDescriptor&& other)
+    : mHandle{other.mHandle}
+{
+    other.mHandle = None;
+}
+
+FileDescriptor&
+FileDescriptor::operator=(FileDescriptor&& other)
+{
+    if (mHandle != None)
+        closeUninterrubtible();
+
+    mHandle       = other.mHandle;
+    other.mHandle = None;
+
+    return *this;
+}
+
+FileDescriptor::~FileDescriptor()
+{
+    if (mHandle != None)
+        try
+        {
+            closeUninterrubtible();
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+}
+
+FileDescriptor::Handle
+FileDescriptor::toNative() const
+{
+    return mHandle;
+}
+
+void
+FileDescriptor::closeUninterrubtible()
+{
+    while (::close(mHandle) != 0)
+        if (errno == EINTR)
+            continue;
+        else
+            throw std::system_error{errno, std::system_category()};
+}
+
+#if defined(BUILD_UNIT_TESTS)
+TEST(FileDescriptor, InvalidArguments)
+{
+    EXPECT_THROW(FileDescriptor(-1), std::invalid_argument);
+}
+#endif /* BUILD_UNIT_TESTS */
---
+++ System/FileDescriptor.hpp
@@ -0,0 +1,23 @@
+#ifndef HEADER_B96B9FB0_AA74_11E7_A376_74D435E5BEA6_INCLUDED
+#define HEADER_B96B9FB0_AA74_11E7_A376_74D435E5BEA6_INCLUDED
+
+namespace System {
+class FileDescriptor
+{
+public:
+    using Handle = int;
+
+    explicit FileDescriptor(Handle fd);
+    FileDescriptor(FileDescriptor&& other);
+    FileDescriptor& operator=(FileDescriptor&& other);
+    ~FileDescriptor();
+
+    Handle toNative() const;
+
+private:
+    void closeUninterrubtible();
+    Handle mHandle;
+};
+} /* namespace System  */
+
+#endif /* end of the header guard */
