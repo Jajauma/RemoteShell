3dd6069344a96969a1be65d4014b093cb5005a86
Jajauma <jajauma@2ch.hk>
2017-10-09 15:38:57 +0300

    Some refactoring

--- ShellClient/Main.cpp
+++ ShellClient/Main.cpp
@@ -8,33 +8,28 @@
 int
 main(int argc, char* argv[])
 {
-    System::FileDescriptor conn;
-
+    System::FileDescriptor clientSocket;
     try
     {
         switch (argc)
         {
         case 2:
-            conn = ShellClient::connect(argv[1]);
+            clientSocket = ShellClient::connect(argv[1]);
             break;
-
         case 3:
-            conn = ShellClient::connect(argv[1], argv[2]);
+            clientSocket = ShellClient::connect(argv[1], argv[2]);
             break;
-
         default:
             std::cerr << "Usage: " << argv[0] << " [<addr>] <port>"
                       << std::endl;
             return EXIT_FAILURE;
         }
-
-        ShellClient::multiplexIOLoop(conn);
+        ShellClient::multiplexIOLoop(clientSocket);
     }
     catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
