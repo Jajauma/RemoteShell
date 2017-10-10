0b7843be16e79dec3739e89651bac79b5edb3390
Jajauma <jajauma@2ch.hk>
2017-10-10 14:46:27 +0300

    Accept and log incoming connections

--- ShellServer/ProcessClientsLoop.cpp
+++ ShellServer/ProcessClientsLoop.cpp
@@ -1,12 +1,60 @@
 #include "ProcessClientsLoop.hpp"
 #include "Cxx/Unused.hpp"
 #include "System/FileDescriptor.hpp"
+#include "System/GetNameInfo.hpp"
 
-#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+
+#include <cstring>
+#include <iostream>
+#include <system_error>
+
+void
+ReapChildProcess(int signalCode)
+{
+    Cxx::Unused(signalCode);
+}
 
 void
 ShellServer::processClientsLoop(System::FileDescriptor& serverSocket)
 {
-    Cxx::Unused(serverSocket);
-    ::pause();
+    struct sigaction act;
+    std::memset(&act, 0, sizeof(act));
+    act.sa_handler = ReapChildProcess;
+
+    auto ret = ::sigaction(SIGCHLD, &act, nullptr);
+    if (ret != 0)
+        throw std::system_error{errno, std::generic_category()};
+
+    while (true)
+    {
+        sockaddr clientAddress;
+        socklen_t clientAddressLength{sizeof(clientAddress)};
+
+        auto ret = ::accept(
+            serverSocket.toNative(), &clientAddress, &clientAddressLength);
+        if (ret == -1)
+            switch (errno)
+            {
+            case EINTR:
+            case ECONNABORTED:
+                continue;
+            case EMFILE:
+            case ENFILE:
+            case ENOBUFS:
+            case ENOMEM:
+                std::cerr << "System running low on resources, can't accept "
+                             "incoming connections"
+                          << std::endl;
+                continue;
+            default:
+                throw std::system_error{errno, std::generic_category()};
+            }
+
+        System::FileDescriptor clientSocket{ret};
+        System::GetNameInfo peerName(&clientAddress, clientAddressLength);
+        std::cerr << "Accepted connection from " << peerName.host() << ":"
+                  << peerName.service() << std::endl;
+    }
 }
